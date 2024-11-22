2ddfc0ad5bf9a5a148434c28a908211604d9636c
kevinsam214 <kevinsam214@gmail.com>
2024-11-22 14:52:53 +0800

    build: main.js add: app.js & styles.css & index.hbs

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function generatePassword(data) {
 
   if (data.uppercase === "on") {
     collection = collection.concat(uppercase.split(''))
-  }
+  }//concat返回一個包含合併後所有元素的新陣列
 
   if (data.lowercase === "on") {
     collection = collection.concat(lowercase.split(''))
@@ -28,7 +28,7 @@ function generatePassword(data) {
     collection = collection.filter(
       char => !exclude.includes(char)
     )
-  }
+  }//符合char => !exclude.includes(char)條件時 為true保留 不符合則為false 不保留
 
   if (data.length < 4 || data.length > 16 || data.length.value === '') {
     return password = 'Please enter the correct password length'
@@ -36,17 +36,17 @@ function generatePassword(data) {
 
   if (data.uppercase !== 'on' && data.lowercase !== 'on' && data.number !== 'on' && data.symbol !== 'on') {
     return password = 'Please select at least one'
-  }
+  }//檢查是否至少選擇了一種類型的字元。如果都沒選，則返回提示訊息。
 
   if (collection.length === 0) {
     return password = 'There is no valid conditions'
-  }
+  }//如果 collection 為空，代表沒有有效條件可生成密碼，返回提示訊息 =>因為data.length.value 如果不為 === '' , 並不代表collection.length不為0 因為可能被exclude過濾掉
 
   for (let i = 0; i < data.length; i++) {
     const collectionIndex = Math.floor(Math.random() * collection.length)
     password += collection[collectionIndex];
   }
   return password;
-};
+};//使用迴圈生成密碼，迴圈次數為密碼長度（data.length）。
 
-module.exports = generatePassword;
+module.exports = generatePassword;//這行程式碼將 generatePassword 函式導出，使其他檔案可以透過 require 引入並使用它。
